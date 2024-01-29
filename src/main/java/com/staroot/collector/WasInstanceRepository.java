db477730964e54a0f2c793cd7558fd08fbcd484b
hiwatt <hiwattc@gmail.com>
2024-01-30 00:38:28 +0900

    2024-01-30

--- src/main/java/com/staroot/collector/WasInstanceRepository.java
+++ src/main/java/com/staroot/collector/WasInstanceRepository.java
@@ -3,7 +3,9 @@ package com.staroot.collector;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 public interface WasInstanceRepository extends CrudRepository<WasInstance,String> {
-    List<ServerInfo> findByHostname(String hostname);
+    List<WasInstance> findByHostname(String hostname);
+    List<WasInstance> findByDomain(String domain);
+    List<WasInstance> findByIp(String ip);
 }
 
 
